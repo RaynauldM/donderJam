fd8d9b59429f23a8df84c1423e1be34dc5bbdaff
RaynauldM <raynauldminkema@gmail.com>
2024-02-03 15:11:49 +0100

    -updated header and nav layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let headerText = 'Welkom bij de donderJam!';
+    import {page} from '$app/stores';
+    $: url = $page.url; 
+</script>
+
+<header>
+    {#if url.pathname == '/'}
+        <h1>{headerText}</h1>
+    {:else}
+        <h1>{url.pathname.slice(1)}</h1>
+    {/if}
+    <nav>
+        <ul>
+            <li><a href="/info">Info</a></li>
+            <li><a href="/meedoen">Ik wil meedoen</a></li>
+            <li><a href="/aandragen">Ik weet een leuk nummer</a></li>
+        </ul>
+        
+    </nav>
+</header>
+
+<slot />
+
+<style>
+    header {
+        text-align: center;
+    }
+    nav ul {
+        display: flex;
+        flex-wrap: wrap;
+        list-style-type: none;
+        justify-content: space-around;
+    }
+
+    a {
+        text-decoration: none;;
+    }
+    
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,5 @@
 
 
-<header>
-    <h1>Welkom bij de donderjam!</h1>
-</header>
-
-<nav>
-    <ul>
-        <li><a href="/info">Info</a></li>
-        <li><a href="/meedoen">Ik wil meedoen</a></li>
-        <li><a href="/aandragen">Ik weet nog een leuk nummer</a></li>
-    </ul>
-    
-</nav>
-
 <div id="setListContainer">
-    <p>Default setlist</p>
+    <p>default setlist output</p>
 </div>
