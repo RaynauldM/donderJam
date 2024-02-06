6b9349bc5e9cdcd9a4f50cec3e789069737a938f
RaynauldM <raynauldminkema@gmail.com>
2024-02-06 20:02:47 +0100

    -huisband route has light security

--- src/routes/huisband/+page.svelte
+++ src/routes/huisband/+page.svelte
@@ -1,1 +1,38 @@
-<h1>default huisband page, inlog pagina</h1>
+<script>
+    const allowedNames = [
+        'raynauld',
+        'jur',
+        'david',
+        'peter'
+    ]
+    $: loggedIn = '';
+    $: name = '';
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (allowedNames.includes(name)) {
+            loggedIn = 'true'
+        }
+    }
+</script>
+
+<h1>Huisband pagina</h1>
+
+
+
+{#if loggedIn == ''}
+    <form on:submit={handleSubmit}>
+        <label>
+            Naam:
+            <input type='text' bind:value={name}>
+        </label>
+        <button type='submit'>Let's go!</button>
+    </form>
+{:else}
+    <h2>logged in</h2>
+{/if}
+
+<style>
+    h2 {
+        color: white;
+    }
+</style>
